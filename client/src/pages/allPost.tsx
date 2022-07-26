76d40c45f870036d39342a5191744dd8c0b125dc
KimJaewon3 <poilkj1po@gmail.com>
2022-07-26 17:10:49 +0900

    feat: 페이지 네이션 컴포넌트 추가

--- client/src/pages/allPost.tsx
+++ client/src/pages/allPost.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Pagination from '../components/ pagination';
 
 type Post = {
   id: number;
@@ -12,12 +13,15 @@ type Post = {
 export default function AllPost() {
   const nav = useNavigate();
   const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState(1);
+  const [postLimit, setPostLimit] = useState(20);
+  const start = (page - 1) * postLimit;
+  const end = start + postLimit;
 
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then(res => {
-        console.log(res.data);
         setPosts(res.data);
       })
       .catch(err => {
@@ -33,17 +37,19 @@ export default function AllPost() {
     <div>
       <h1>전체 게시판</h1>
       <ul>
-        <li>
-          <p>제목</p>
-          <p>작성자</p>
-        </li>
-        {posts.map(post => (
-          <li key={post.id} onClick={() => handlePostClick(post)}>
-            <p>{post.title}</p>
+        {posts.slice(start, end).map(post => (
+          <li key={post.id}>
+            <p onClick={() => handlePostClick(post)}>{post.title}</p>
             <p>작성자 {post.userId}</p>
           </li>
         ))}
       </ul>
+      <Pagination 
+        contentsCount={posts.length}
+        postLimit={postLimit}
+        setPage={setPage}
+        page={page}
+      />
     </div>
   );
 }
